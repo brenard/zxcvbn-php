ae5b01ad9f3de093f5b5fde1b98c8aa6ec9361c5
Benjamin Renard <brenard@easter-eggs.com>
2021-12-16 20:08:57 +0100

    autoload.php: Fix errors reported by phpcs

--- debian/autoload.php
+++ debian/autoload.php
@@ -1,12 +1,14 @@
 <?php
 
-spl_autoload_register(function($class) {
-    if (strpos($class, 'ZxcvbnPhp\\') != 0) return false;
-    include(
-	__DIR__. DIRECTORY_SEPARATOR .
-        str_replace(
-            "\\", DIRECTORY_SEPARATOR,
-            substr($class, 10)
-        ).".php"
-    );
+spl_autoload_register(function ($class) {
+    if (strpos($class, 'ZxcvbnPhp\\') != 0) {
+        include(
+            __DIR__ . DIRECTORY_SEPARATOR .
+            str_replace(
+                "\\",
+                DIRECTORY_SEPARATOR,
+                substr($class, 10)
+            ) . ".php"
+        );
+    }
 });
